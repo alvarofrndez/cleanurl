91f70f4569edd0a8a4999e234644072a57b95f7e
alvaro <alvarofdayala@gmail.com>
2024-07-31 13:58:19 +0200

    card of url

--- client/src/components/ShorterComponent.vue
+++ client/src/components/ShorterComponent.vue
@@ -1,16 +1,18 @@
 <script setup>
     import { ref } from 'vue'
     import { useShorterStore } from '@/stores/shorter'
+    import { useGlobalStore } from '@/stores/global'
 
-    const url = ref()
     const shorter_s = useShorterStore()
+    const global_s = useGlobalStore()
+    const url = ref()
     const result = ref({})
 
     async function shortURL(){
         const response = await shorter_s.shortUrl(url.value)
 
         if(response.ok){
-            console.log(response.data)
+            result.value = response.data
         }
     }
 </script>
@@ -20,7 +22,12 @@
         <h1>Introduzca la URL</h1>
         <input type='text' placeholder='https://dominio.com/url-a-acortar' v-model='url' required/>
         <button type='submit'>Obtener link</button>
-        <div class='container-result' v-if="result.short_url"></div>
+        <div class='container-result' v-if="result.short_url">
+            <span>{{ result.short_url.created_at }}</span>
+            <span>{{ result.short_url.original_url }}</span>
+            <span>{{ result.short_url.short_code }}</span>
+            <span>{{ global_s.API_URL + result.short_url.short_code }}</span>
+        </div>
     </form>
 </template>
 
